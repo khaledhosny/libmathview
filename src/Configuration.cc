a21a85f1bca117368ce9c441d79004a76a3d5627
lpadovan <lpadovan>
2000-12-13 18:25:35 +0000

    Added files: auto/config.dirs : a small file with a PKGDATADIR definition
    Added files:
    auto/config.dirs : a small file with a PKGDATADIR definition
    src/Configuration.cc : configuration manager
    src/Configuration.hh : configuration manager header file

---
+++ src/Configuration.cc
@@ -0,0 +1,175 @@
+// Copyright (C) 2000, Luca Padovani <luca.padovani@cs.unibo.it>.
+// 
+// This file is part of GtkMathView, a Gtk widget for MathML.
+// 
+// GtkMathView is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// GtkMathView is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GtkMathView; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// For details, see the GtkMathView World-Wide-Web page,
+// http://cs.unibo.it/~lpadovan/mml-widget/, or send a mail to
+// <luca.padovani@cs.unibo.it>
+
+#include <config.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "MathEngine.hh"
+#include "StringUnicode.hh"
+#include "Configuration.hh"
+#include "AttributeParser.hh"
+#include "MathMLParseFile.hh"
+#include "ValueConversion.hh"
+
+Configuration::Configuration(void)
+{
+  colorSet = linkColorSet = selectColorSet = false;
+}
+
+Configuration::~Configuration()
+{
+}
+
+bool
+Configuration::Load(const char* confPath)
+{
+  assert(confPath != NULL);
+
+  MathEngine::logger(LOG_DEBUG, "loading configuration from `%s'...", confPath);
+
+  mDOMDocRef doc = MathMLParseFile(confPath, false);
+  if (doc == NULL) return false;
+
+  mDOMNodeRef root = mdom_doc_get_root_node(doc);
+  if (root == NULL) {
+    mdom_doc_free(doc);
+    MathEngine::logger(LOG_WARNING, "configuration file `%s' has no root node", confPath);
+    return false;
+  }
+
+  if (!mdom_string_eq(mdom_node_get_name(root), DOM_CONST_STRING("math-engine-configuration"))) {
+    mdom_doc_free(doc);
+    MathEngine::logger(LOG_WARNING, "configuration file `%s': could not find root element", confPath);
+    return false;
+  }
+
+  ParseConfiguration(root);
+
+  mdom_doc_free(doc);
+
+  return true;
+}
+
+void
+Configuration::ParseConfiguration(mDOMNodeRef node)
+{
+  assert(node != NULL);
+  
+  for (mDOMNodeRef p = mdom_node_get_first_child(node);
+       p != NULL;
+       p = mdom_node_get_next_sibling(p)) {
+    mDOMConstStringRef name = mdom_node_get_name(p);
+    
+    if (mdom_string_eq(name, DOM_CONST_STRING("dictionary-path"))) {
+      mDOMStringRef path = mdom_node_get_content(p);
+      if (path != NULL) {
+	MathEngine::logger(LOG_DEBUG, "found dictionary path `%s'", C_STRING(path));
+	dictionaries.Append(new StringC(C_STRING(path)));
+      }
+    } else if (mdom_string_eq(name, DOM_CONST_STRING("font-configuration-path"))) {
+      mDOMStringRef path = mdom_node_get_content(p);
+      if (path != NULL) {
+	MathEngine::logger(LOG_DEBUG, "found font configuration path `%s'", C_STRING(path));
+	fonts.Append(new StringC(C_STRING(path)));
+      }
+    } else if (mdom_string_eq(name, DOM_CONST_STRING("entities-table-path"))) {
+      mDOMStringRef path = mdom_node_get_content(p);
+      if (path != NULL) {
+	MathEngine::logger(LOG_DEBUG, "found entities table path `%s'", C_STRING(path));
+	entities.Append(new StringC(C_STRING(path)));
+      }
+    } else if (mdom_string_eq(name, DOM_CONST_STRING("t1-config-file"))) {
+      mDOMStringRef path = mdom_node_get_content(p);
+      if (path != NULL && t1Configs.GetSize() == 0) {
+	MathEngine::logger(LOG_DEBUG, "found t1lib config path `%s'", C_STRING(path));
+	t1Configs.Append(new StringC(C_STRING(path)));
+      }
+    } else if (mdom_string_eq(name, DOM_CONST_STRING("font-size"))) {
+      mDOMStringRef attr = mdom_node_get_attribute(p, DOM_CONST_STRING("size"));
+      if (attr == NULL) {
+	MathEngine::logger(LOG_WARNING, "malformed `font-size' element, cannot find `size' attribute");
+      } else {
+	fontSize = atoi(C_STRING(attr));
+	MathEngine::logger(LOG_DEBUG, "default font size set to %d points", fontSize);
+	fontSizeSet = true;
+      }
+    } else if (mdom_string_eq(name, DOM_CONST_STRING("color"))) {
+      colorSet = ParseColor(p, foreground, background);
+      if (colorSet) MathEngine::logger(LOG_DEBUG, "default color set to %06x %06x", foreground, background);
+      else MathEngine::logger(LOG_WARNING, "color parsing error in configuration file");
+    } else if (mdom_string_eq(name, DOM_CONST_STRING("link-color"))) {
+      linkColorSet = ParseColor(p, linkForeground, linkBackground);
+      if (linkColorSet) MathEngine::logger(LOG_DEBUG, "default link color set to %06x %06x", linkForeground, linkBackground);
+      else MathEngine::logger(LOG_WARNING, "color parsing error in configuration file");
+    } else if (mdom_string_eq(name, DOM_CONST_STRING("select-color"))) {
+      selectColorSet = ParseColor(p, selectForeground, selectBackground);
+      if (selectColorSet) MathEngine::logger(LOG_DEBUG, "default selection color set to %06x %06x", selectForeground, selectBackground);
+      else MathEngine::logger(LOG_WARNING, "color parsing error in configuration file");
+    } else if (!mdom_node_is_blank(p)) {
+      MathEngine::logger(LOG_WARNING, "unrecognized entry %s in configuration file (ignored)",
+			 C_STRING(mdom_node_get_name(p)));
+    }
+  }
+}
+
+bool
+Configuration::ParseColor(mDOMNodeRef node, RGBValue& f, RGBValue& b)
+{
+  assert(node != NULL);
+
+  mDOMStringRef fs = mdom_node_get_attribute(node, DOM_STRING("foreground"));
+  mDOMStringRef bs = mdom_node_get_attribute(node, DOM_STRING("background"));
+
+  if (fs == NULL || bs == NULL) {
+    MathEngine::logger(LOG_WARNING, "malformed `%s' element in configuration file",
+		       mdom_node_get_name(node));
+    return false;
+  }
+
+  StringC fss(C_STRING(fs));
+  StringC bss(C_STRING(bs));
+
+  StringTokenizer fst(fss);
+  StringTokenizer bst(bss);
+
+  const Value* fv = colorParser(fst);
+  const Value* bv = colorParser(bst);
+
+  if (fv == NULL || bv == NULL) {
+    delete fv;
+    delete bv;
+
+    MathEngine::logger(LOG_WARNING, "malformed color attribute in configuration file, `%s' element",
+		       mdom_node_get_name(node));
+
+    return false;
+  }
+
+  f = ToRGB(fv);
+  b = ToRGB(bv);
+
+  delete fv;
+  delete bv;
+
+  return true;
+}
---
+++ src/Configuration.hh
@@ -0,0 +1,87 @@
+// Copyright (C) 2000, Luca Padovani <luca.padovani@cs.unibo.it>.
+// 
+// This file is part of GtkMathView, a Gtk widget for MathML.
+// 
+// GtkMathView is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// GtkMathView is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GtkMathView; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// For details, see the GtkMathView World-Wide-Web page,
+// http://cs.unibo.it/~lpadovan/mml-widget/, or send a mail to
+// <luca.padovani@cs.unibo.it>
+
+#ifndef Configuration_hh
+#define Configuration_hh
+
+#include <config.h>
+
+#include <minidom.h>
+
+#include "scaled.hh"
+#include "String.hh"
+#include "RGBValue.hh"
+#include "Container.hh"
+
+class Configuration {
+public:
+  Configuration(void);
+  ~Configuration();
+
+  bool Load(const char*);
+
+  const Container<String*>& GetDictionaries(void) const { return dictionaries; }
+  const Container<String*>& GetFonts(void) const { return fonts; }
+  const Container<String*>& GetEntities(void) const { return entities; }
+  const Container<String*>& GetT1ConfigFiles(void) const { return t1Configs; }
+
+  bool     HasFontSize(void) const { return fontSizeSet; }
+  unsigned GetFontSize(void) const { return HasFontSize() ? fontSize : DEFAULT_FONT_SIZE; }
+
+  bool     HasColor(void) const { return colorSet; }
+  RGBValue GetForeground(void) const { return HasColor() ? foreground : DEFAULT_FOREGROUND; }
+  RGBValue GetBackground(void) const { return HasColor() ? background : DEFAULT_BACKGROUND; }
+
+  bool     HasLinkColor(void) const { return linkColorSet; }
+  RGBValue GetLinkForeground(void) const { return HasLinkColor() ? linkForeground : DEFAULT_LINK_FOREGROUND; }
+  RGBValue GetLinkBackground(void) const { return HasLinkColor() ? linkBackground : DEFAULT_LINK_BACKGROUND; }
+
+  bool     HasSelectColor(void) const { return selectColorSet; }
+  RGBValue GetSelectForeground(void) const { return HasSelectColor() ? selectForeground : DEFAULT_SELECT_FOREGROUND; }
+  RGBValue GetSelectBackground(void) const { return HasSelectColor() ? selectBackground : DEFAULT_SELECT_BACKGROUND; }
+
+private:
+  void ParseConfiguration(mDOMNodeRef);
+  bool ParseColor(mDOMNodeRef, RGBValue&, RGBValue&);
+
+  Container<String*> dictionaries;
+  Container<String*> fonts;
+  Container<String*> entities;
+  Container<String*> t1Configs;
+
+  bool     fontSizeSet;
+  unsigned fontSize;
+
+  bool     colorSet;
+  RGBValue foreground;
+  RGBValue background;
+
+  bool     linkColorSet;
+  RGBValue linkForeground;
+  RGBValue linkBackground;
+
+  bool     selectColorSet;
+  RGBValue selectForeground;
+  RGBValue selectBackground;
+};
+
+#endif // Configuration_hh
