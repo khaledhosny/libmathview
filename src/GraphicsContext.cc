7cbe9c5e8cd467cbdd41c21f5c9b97774d956ed8
Luca Padovani <luca@src.gnome.org>
2002-04-03 16:18:19 +0000

    removed #ifdef HAVE_CONFIG_H

--- src/GraphicsContext.cc
+++ src/GraphicsContext.cc
@@ -20,9 +20,7 @@
 // http://cs.unibo.it/~lpadovan/mml-widget, or send a mail to
 // <luca.padovani@cs.unibo.it>
 
-#ifdef HAVE_CONFIG_H
 #include <config.h>
-#endif
 
 #include "GraphicsContext.hh"
 
