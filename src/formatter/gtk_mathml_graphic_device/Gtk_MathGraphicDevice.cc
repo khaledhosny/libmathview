efe17f079c0f2355782b848649413d36c8584984
Luca Padovani <luca@src.gnome.org>
2003-08-08 11:47:03 +0000

    license added few corrections to assure compilation
    * license added
    * few corrections to assure compilation

--- src/formatter/gtk_mathml_graphic_device/Gtk_MathGraphicDevice.cc
+++ src/formatter/gtk_mathml_graphic_device/Gtk_MathGraphicDevice.cc
@@ -1,26 +1,24 @@
-/* This file is part of MFD, a Math Formatting Device for various
- * contexts/backends 
- * 
- * Copyright (C) 2003 Luca Padovani <lpadovan@cs.unibo.it>,
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- * For more information, please visit the project's home page
- * http://www.cs.unibo.it/~lpadovan/mfd/
- * or send an email to <lpadovan@cs.unibo.it>
- */
+// Copyright (C) 2000-2003, Luca Padovani <luca.padovani@cs.unibo.it>.
+//
+// This file is part of GtkMathView, a Gtk widget for MathML.
+// 
+// GtkMathView is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// GtkMathView is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GtkMathView; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// For details, see the GtkMathView World-Wide-Web page,
+// http://helm.cs.unibo.it/mml-widget, or send a mail to
+// <luca.padovani@cs.unibo.it>
 
 #include <config.h>
 
