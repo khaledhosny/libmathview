12a805472da2800e77d391b30b7de7aa67dc93c8
Luca Padovani <luca@src.gnome.org>
2003-12-17 15:14:43 +0000

    added .h files
    * added .h files

---
+++ src/common/for_each_if.h
@@ -0,0 +1,41 @@
+// Copyright (C) 2000-2002, Luca Padovani <luca.padovani@cs.unibo.it>.
+//
+// This file is part of GtkMathView, a Gtk widget for MathML.
+// 
+// GtkMathView is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// GtkMathView is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GtkMathView; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// For details, see the GtkMathView World-Wide-Web page,
+// http://www.cs.unibo.it/helm/mml-widget, or send a mail to
+// <luca.padovani@cs.unibo.it>
+
+#ifndef __for_each_if__
+#define __for_each_if__
+
+template <typename InputIterator,
+	  typename Predicate,
+	  typename UnaryFunction>
+UnaryFunction for_each_if(InputIterator first, InputIterator last,
+			  Predicate p, UnaryFunction f)
+{
+  while (first != last)
+    {
+      if (p(*first)) f(*first);
+      ++first;
+    }
+
+  return f;
+}
+
+#endif // __for_each_if__
