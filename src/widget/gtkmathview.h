db9ffd2e0800e18d49e979a5884d9e70bce2dacc
Luca Padovani <luca@src.gnome.org>
2004-05-03 15:27:03 +0000

    engine now a library of its own introduced several versions of the GTK API
    * engine now a library of its own
    * introduced several versions of the GTK API for different frontends

--- src/widget/gtkmathview.h
+++ src/widget/gtkmathview.h
@@ -1,83 +1,58 @@
-/*
- * Copyright (C) 2000, Luca Padovani <luca.padovani@cs.unibo.it>.
- * 
- * This file is part of GtkMathView, a Gtk widget for MathML.
- * 
- * GtkMathView is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * GtkMathView is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with GtkMathView; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
- * For details, see the GtkMathView World-Wide-Web page,
- * http://cs.unibo.it/~lpadovan/mml-widget, or send a mail to
- * <luca.padovani@cs.unibo.it>
- */
+// Copyright (C) 2000-2004, Luca Padovani <luca.padovani@cs.unibo.it>.
+//
+// This file is part of GtkMathView, a Gtk widget for MathML.
+// 
+// GtkMathView is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// GtkMathView is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GtkMathView; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// For details, see the GtkMathView World-Wide-Web page,
+// http://helm.cs.unibo.it/mml-widget/, or send a mail to
+// <lpadovan@cs.unibo.it>
 
-#ifndef gtkmathview_h
-#define gtkmathview_h
+#ifndef __gtkmathview_h__
+#define __gtkmathview_h__
 
-#include "defs.h"
-#include "c_customXmlReader.h"
-
-#include <gtk/gtkframe.h>
-#include <gtk/gtkeventbox.h>
-#include <gtk/gtkdrawingarea.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
-#define GTK_TYPE_MATH_VIEW         (gtk_math_view_get_type())
-#define GTK_MATH_VIEW(obj)         GTK_CHECK_CAST(obj, GTK_TYPE_MATH_VIEW, GtkMathView)
-#define GTK_MATH_VIEW_CLASS(klass) GTK_CHECK_CLASS_CAST(klass, GTK_TYPE_MATH_VIEW, GtkMathViewClass)
-#define GTK_IS_MATH_VIEW(obj)      GTK_CHECK_TYPE(obj, GTK_TYPE_MATH_VIEW)
-
-  typedef struct _GtkMathView       GtkMathView;
-  typedef struct _GtkMathViewClass  GtkMathViewClass;
-  typedef struct _c_customXmlReader GtkMathViewReader;
-  typedef void*                     GtkMathViewReaderData;
-  typedef void*                     GtkMathViewElementId;
-
-  GtkType    	 gtk_math_view_get_type(void);
-  GtkWidget* 	 gtk_math_view_new(GtkAdjustment*, GtkAdjustment*);
-  gboolean       gtk_math_view_freeze(GtkMathView*);
-  gboolean       gtk_math_view_thaw(GtkMathView*);
-  gboolean       gtk_math_view_load_uri(GtkMathView*, const gchar*);
-  gboolean       gtk_math_view_load_reader(GtkMathView*, GtkMathViewReader*, GtkMathViewReaderData);
-  gboolean       gtk_math_view_load_root(GtkMathView*, GtkMathViewElementId);
-  void           gtk_math_view_unload(GtkMathView*);
-  void           gtk_math_view_select(GtkMathView*, GtkMathViewElementId);
-  void           gtk_math_view_unselect(GtkMathView*, GtkMathViewElementId);
-  gboolean       gtk_math_view_is_selected(GtkMathView*, GtkMathViewElementId);
-  gboolean       gtk_math_view_get_element_at(GtkMathView*, gint, gint, GtkMathViewElementId*);
-  gboolean       gtk_math_view_get_element_location(GtkMathView*, GtkMathViewElementId, gint*, gint*, GdkRectangle*);
-  gboolean       gtk_math_view_get_char_at(GtkMathView*, gint, gint, GtkMathViewElementId*, gint*);
-  gboolean       gtk_math_view_get_char_location(GtkMathView*, GtkMathViewElementId, gint, gint*, gint*, GdkRectangle*);
-  gint      	 gtk_math_view_get_width(GtkMathView*);
-  gint      	 gtk_math_view_get_height(GtkMathView*);
-  void       	 gtk_math_view_get_top(GtkMathView*, gint*, gint*);
-  void       	 gtk_math_view_set_top(GtkMathView*, gint, gint);
-  void       	 gtk_math_view_set_adjustments(GtkMathView*, GtkAdjustment*, GtkAdjustment*);
-  GtkAdjustment* gtk_math_view_get_hadjustment(GtkMathView*);
-  GtkAdjustment* gtk_math_view_get_vadjustment(GtkMathView*);
-  GdkPixmap*     gtk_math_view_get_buffer(GtkMathView*);
-  GtkDrawingArea* gtk_math_view_get_drawing_area(GtkMathView*);
-  void           gtk_math_view_set_font_size(GtkMathView*, guint);
-  guint          gtk_math_view_get_font_size(GtkMathView*);
-  void           gtk_math_view_set_log_verbosity(GtkMathView*, gint);
-  gint           gtk_math_view_get_log_verbosity(GtkMathView*);
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-#endif /* gtkmathview_h */
-
+#define gtk_math_view_get_type GTKMATHVIEW_METHOD_NAME(get_type)
+#define gtk_math_view_new GTKMATHVIEW_METHOD_NAME(new)
+#define gtk_math_view_freeze GTKMATHVIEW_METHOD_NAME(freeze)
+#define gtk_math_view_thaw GTKMATHVIEW_METHOD_NAME(thaw)
+#if GTKMATHVIEW_USES_CUSTOM_READER
+#define gtk_math_view_load_reader GTKMATHVIEW_METHOD_NAME(load_reader)
+#else
+#define gtk_math_view_load_uri GTKMATHVIEW_METHOD_NAME(load_uri)
+#define gtk_math_view_load_root GTKMATHVIEW_METHOD_NAME(load_root)
+#endif
+#define gtk_math_view_unload GTKMATHVIEW_METHOD_NAME(unload)
+#define gtk_math_view_select GTKMATHVIEW_METHOD_NAME(select)
+#define gtk_math_view_unselect GTKMATHVIEW_METHOD_NAME(unselect)
+#define gtk_math_view_is_selected GTKMATHVIEW_METHOD_NAME(is_selected)
+#define gtk_math_view_get_element_at GTKMATHVIEW_METHOD_NAME(get_element_at)
+#define gtk_math_view_get_element_location GTKMATHVIEW_METHOD_NAME(get_element_location)
+#define gtk_math_view_get_char_at GTKMATHVIEW_METHOD_NAME(get_char_at)
+#define gtk_math_view_get_char_location GTKMATHVIEW_METHOD_NAME(get_char_location)
+#define gtk_math_view_get_width GTKMATHVIEW_METHOD_NAME(get_width)
+#define gtk_math_view_get_height GTKMATHVIEW_METHOD_NAME(get_height)
+#define gtk_math_view_get_top GTKMATHVIEW_METHOD_NAME(get_top)
+#define gtk_math_view_set_top GTKMATHVIEW_METHOD_NAME(set_top)
+#define gtk_math_view_set_adjustments GTKMATHVIEW_METHOD_NAME(set_adjustments)
+#define gtk_math_view_get_hadjustment GTKMATHVIEW_METHOD_NAME(get_hadjustment)
+#define gtk_math_view_get_vadjustment GTKMATHVIEW_METHOD_NAME(get_vadjustment)
+#define gtk_math_view_get_buffer GTKMATHVIEW_METHOD_NAME(get_buffer)
+#define gtk_math_view_get_drawing_area GTKMATHVIEW_METHOD_NAME(get_drawing_area)
+#define gtk_math_view_set_font_size GTKMATHVIEW_METHOD_NAME(set_font_size)
+#define gtk_math_view_get_font_size GTKMATHVIEW_METHOD_NAME(get_font_size)
+#define gtk_math_view_set_log_verbosity GTKMATHVIEW_METHOD_NAME(set_log_verbosity)
+#define gtk_math_view_get_log_verbosity GTKMATHVIEW_METHOD_NAME(get_log_verbosity)
+     
+#endif /* __gtkmathview_h__ */
---
+++ src/widget/gtkmathview_common.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2000, Luca Padovani <luca.padovani@cs.unibo.it>.
+ * 
+ * This file is part of GtkMathView, a Gtk widget for MathML.
+ * 
+ * GtkMathView is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * GtkMathView is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GtkMathView; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * For details, see the GtkMathView World-Wide-Web page,
+ * http://cs.unibo.it/~lpadovan/mml-widget, or send a mail to
+ * <luca.padovani@cs.unibo.it>
+ */
+
+#ifndef gtkmathview_h
+#define gtkmathview_h
+
+#include "defs.h"
+
+#include <gtk/gtkframe.h>
+#include <gtk/gtkeventbox.h>
+#include <gtk/gtkdrawingarea.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#define GTK_TYPE_MATH_VIEW         (gtk_math_view_get_type())
+#define GTK_MATH_VIEW(obj)         GTK_CHECK_CAST(obj, GTK_TYPE_MATH_VIEW, GtkMathView)
+#define GTK_MATH_VIEW_CLASS(klass) GTK_CHECK_CLASS_CAST(klass, GTK_TYPE_MATH_VIEW, GtkMathViewClass)
+#define GTK_IS_MATH_VIEW(obj)      GTK_CHECK_TYPE(obj, GTK_TYPE_MATH_VIEW)
+
+  typedef struct _GtkMathView       GtkMathView;
+  typedef struct _GtkMathViewClass  GtkMathViewClass;
+  typedef struct _c_customXmlReader GtkMathViewReader;
+  typedef void*                     GtkMathViewReaderData;
+
+#if GTKMATHVIEW_USES_CUSTOM_READER
+#include "c_customXmlReader.h"
+#define GTKMATHVIEW_METHOD_NAME(name) gtk_math_view_##name##__##custom_reader
+  typedef void*                     GtkMathViewElementId;
+#elif GTKMATHVIEW_USES_LIBXML2
+#include <libxml/tree.h>
+#define GTKMATHVIEW_METHOD_NAME(name) gtk_math_view_##name##__##libxml2
+  typedef xmlElement*               GtkMathViewElementId;
+#elif GTKMATHVIEW_USES_GMETADOM
+#include <gdome.h>
+#define GTKMATHVIEW_METHOD_NAME(name) gtk_math_view_##name##__##gmetadom
+  typedef GdomeElement*             GtkMathViewElementId;
+#endif
+
+  GtkType    	 GTKMATHVIEW_METHOD_NAME(get_type)(void);
+  GtkWidget* 	 GTKMATHVIEW_METHOD_NAME(new)(GtkAdjustment*, GtkAdjustment*);
+  gboolean       GTKMATHVIEW_METHOD_NAME(freeze)(GtkMathView*);
+  gboolean       GTKMATHVIEW_METHOD_NAME(thaw)(GtkMathView*);
+#if GTKMATHVIEW_USES_CUSTOM_READER
+  gboolean       GTKMATHVIEW_METHOD_NAME(load_reader)(GtkMathView*, GtkMathViewReader*, GtkMathViewReaderData);
+#else
+  gboolean       GTKMATHVIEW_METHOD_NAME(load_uri)(GtkMathView*, const gchar*);
+  gboolean       GTKMATHVIEW_METHOD_NAME(load_root)(GtkMathView*, GtkMathViewElementId);
+#endif
+  void           GTKMATHVIEW_METHOD_NAME(unload)(GtkMathView*);
+  void           GTKMATHVIEW_METHOD_NAME(select)(GtkMathView*, GtkMathViewElementId);
+  void           GTKMATHVIEW_METHOD_NAME(unselect)(GtkMathView*, GtkMathViewElementId);
+  gboolean       GTKMATHVIEW_METHOD_NAME(is_selected)(GtkMathView*, GtkMathViewElementId);
+  gboolean       GTKMATHVIEW_METHOD_NAME(get_element_at)(GtkMathView*, gint, gint, GtkMathViewElementId*);
+  gboolean       GTKMATHVIEW_METHOD_NAME(get_element_location)(GtkMathView*, GtkMathViewElementId, gint*, gint*, GdkRectangle*);
+  gboolean       GTKMATHVIEW_METHOD_NAME(get_char_at)(GtkMathView*, gint, gint, GtkMathViewElementId*, gint*);
+  gboolean       GTKMATHVIEW_METHOD_NAME(get_char_location)(GtkMathView*, GtkMathViewElementId, gint, gint*, gint*, GdkRectangle*);
+  gint      	 GTKMATHVIEW_METHOD_NAME(get_width)(GtkMathView*);
+  gint      	 GTKMATHVIEW_METHOD_NAME(get_height)(GtkMathView*);
+  void       	 GTKMATHVIEW_METHOD_NAME(get_top)(GtkMathView*, gint*, gint*);
+  void       	 GTKMATHVIEW_METHOD_NAME(set_top)(GtkMathView*, gint, gint);
+  void       	 GTKMATHVIEW_METHOD_NAME(set_adjustments)(GtkMathView*, GtkAdjustment*, GtkAdjustment*);
+  GtkAdjustment* GTKMATHVIEW_METHOD_NAME(get_hadjustment)(GtkMathView*);
+  GtkAdjustment* GTKMATHVIEW_METHOD_NAME(get_vadjustment)(GtkMathView*);
+  GdkPixmap*     GTKMATHVIEW_METHOD_NAME(get_buffer)(GtkMathView*);
+  GtkDrawingArea* GTKMATHVIEW_METHOD_NAME(get_drawing_area)(GtkMathView*);
+  void           GTKMATHVIEW_METHOD_NAME(set_font_size)(GtkMathView*, guint);
+  guint          GTKMATHVIEW_METHOD_NAME(get_font_size)(GtkMathView*);
+  void           GTKMATHVIEW_METHOD_NAME(set_log_verbosity)(GtkMathView*, gint);
+  gint           GTKMATHVIEW_METHOD_NAME(get_log_verbosity)(GtkMathView*);
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* gtkmathview_h */
+
