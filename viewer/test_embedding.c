f2bd70eea94aaf3d36f23633d84b12afc3276484
Luca Padovani <luca@src.gnome.org>
2004-07-05 08:50:20 +0000

    added test for loading with libxml2 added quit signals for test
    * added test for loading with libxml2
    * added quit signals for test applications

--- viewer/test_embedding.c
+++ viewer/test_embedding.c
@@ -1,9 +1,31 @@
+// Copyright (C) 2000-2004, Luca Padovani <luca.padovani@cs.unibo.it>.
+//
+// This file is part of GtkMathView, a Gtk widget for MathML.
+// 
+// GtkMathView is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// GtkMathView is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GtkMathView; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// For details, see the GtkMathView World-Wide-Web page,
+// http://helm.cs.unibo.it/mml-widget/, or send a mail to
+// <lpadovan@cs.unibo.it>
 
 #include <gtk/gtk.h>
 
 #include "gtkmathview_libxml2.h"
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
   GtkWidget *window;
   GtkTextBuffer* buffer;
@@ -19,8 +41,10 @@ int main(int argc, char *argv[])
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_widget_show  (window);
 
-  gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-    
+  g_signal_connect(GTK_OBJECT(window), 
+		   "delete_event", 
+		   (GtkSignalFunc) gtk_main_quit, NULL);
+
   buffer = gtk_text_buffer_new(NULL);
   gtk_text_buffer_set_text(GTK_TEXT_BUFFER(buffer), "This kind of annotation is useful when something other than the \
 default rendering of the content encoding is desired. For example, by \
@@ -56,7 +80,6 @@ the renderer.", -1);
     
   gtk_widget_show (text_view);
   gtk_widget_show (math_view);
-  gtk_widget_show (window);
     
   gtk_main ();
   
