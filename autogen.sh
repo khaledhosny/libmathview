d11048522f5b050b62dfb858ceca75e4ebf8d402
Hubert Figuiere <hub@src.gnome.org>
2004-07-23 00:10:13 +0000

    Enforce automake 1.7 use in autogen.sh until further changes to work with
    	* autogen.sh: Enforce automake 1.7 use in autogen.sh until
    	further changes to work with older release.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+automake --version | perl -ne 'if (/\(GNU automake\) ([0-9].[0-9])/) {print;  if ($1 < 1.7) {exit 1;}}'
+
+if [ $? -ne 0 ]; then
+    echo "Error: you need automake 1.7 or later.  Please upgrade."
+    exit 1
+fi
+
 aclocal
 libtoolize --force
 autoheader
