1da211a03ed607f67a33a4abe16cb69b297dd55d
Luca Padovani <luca@src.gnome.org>
2007-05-08 16:59:54 +0000

    updated homepage after release
    * updated homepage after release
    
    svn path=/trunk/; revision=1144

--- homepage/mathmlsvg.sh
+++ homepage/mathmlsvg.sh
@@ -4,8 +4,8 @@ if test `uname` == "Darwin"; then
   BUILD_PATH=~/Projects/Builds/gtkmathview_svg
   SOURCE_PATH=~/Projects/gtkmathview
 else
-  BUILD_PATH=~/projects/BUILDS/gtkmathview_svg
-  SOURCE_PATH=~/projects/gtkmathview
+  BUILD_PATH=/localdisk/luca/Builds/gtkmathview_gmetadom_full
+  SOURCE_PATH=~/Projects/gtkmathview
 fi
 
 MATHMLSVG="$BUILD_PATH/mathmlsvg/mathmlsvg --config=$SOURCE_PATH/config/gtkmathview.conf.xml.debug"
