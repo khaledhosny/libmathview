ca3cf851547428dffc80f56ab85b5d89ecacf40a
Khaled Hosny <khaledhosny@eglug.org>
2016-04-17 04:26:21 +0200

    Minor

--- src/backend/MathShaper.cc
+++ src/backend/MathShaper.cc
@@ -52,11 +52,11 @@ MathShaper::shape(ShapingContext& context) const
   hb_buffer_set_script(buffer, hb_script_from_string("Math", -1));
   hb_buffer_add_utf32(buffer, (const uint32_t*) source.data(), source.length(), 0, source.length());
 
-  int scriptLevel = context.getScriptLevel();
+  unsigned scriptLevel = context.getScriptLevel();
   if (scriptLevel > 0)
     {
       hb_feature_t features[] = {
-        { HB_TAG('s','s','t','y'), (unsigned)scriptLevel, 0, (unsigned)-1 },
+        { HB_TAG('s','s','t','y'), scriptLevel, 0, (unsigned)-1 },
         { 0, 0, 0, 0 }
       };
       hb_shape(font, buffer, features, 1);
